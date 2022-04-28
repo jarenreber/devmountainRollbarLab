865560a7f76f842345b8cb2e5bb9dd23ab80ba38
jarenreber <jaren.c.reber@gmail.com>
2022-04-28 14:59:30 -0600

    pathjoin

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const path = require('path')
 const app = express()
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, '../client'))
+    res.sendFile(path.join('../server', '../client'))
 })
 
 const port = process.env.PORT || 4646
