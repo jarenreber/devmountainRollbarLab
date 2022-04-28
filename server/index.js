246075664b09369b66be3569971a969787f4539f
jarenreber <jaren.c.reber@gmail.com>
2022-04-28 15:08:36 -0600

    rollbar added

--- server/index.js
+++ server/index.js
@@ -9,6 +9,16 @@ app.get("/", (req, res) => {
 
 const port = process.env.PORT || 4646
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: 'eacb71e48ab747d0adb5e82a7524c00f',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
 
 
 app.listen(port, () => {
